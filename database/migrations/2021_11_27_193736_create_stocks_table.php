8b17ebf516acab79f6f5144ce7977a72c09716f2
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2021-11-28 02:49:53 +0600

    Stock Controller, Model and Migration

---
+++ database/migrations/2021_11_27_193736_create_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('product_id');
+            $table->integer('receipts');
+            $table->integer('sell');
+            $table->integer('balance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
