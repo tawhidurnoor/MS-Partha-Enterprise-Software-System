1a3c505d560d36443f2c0afd838bdbf30502cb28
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2021-11-30 21:19:14 +0600

    Sell Controller, Migration & Model

--- resources/views/Clients/index.blade.php
+++ resources/views/Clients/index.blade.php
@@ -174,7 +174,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Edit Product</h4>
+                <h4 class="modal-title">Edit Client Info</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
