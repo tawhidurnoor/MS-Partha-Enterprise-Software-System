64b1668544ca5167ac334b17b69befba9a700efa
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-01-19 23:28:08 +0600

    Invoice Print

--- resources/views/Sell/edit.blade.php
+++ resources/views/Sell/edit.blade.php
@@ -25,109 +25,113 @@
                     </div>
                     @endif
 
-                    <div class="row">
 
-                        <div class="col-xl-6 col-md-6">
-                            <div class="card user-card-full" style="height: 150px;">
-                                <div class="row m-l-0 m-r-0">
-                                    <div class="col-sm-4 bg-c-lite-green user-profile" style="height: 150px;">
-                                        <div class="card-block text-center text-white">
-                                            <h6 class="f-w-600">
-                                                {{$client->client_name}}
-                                            </h6>
-                                            <button class="btn edit_button" style="color: white;">
-                                                <i class="feather icon-edit m-t-10 f-16"></i>
-                                            </button>
+                    <div id="printableArea">
+
+
+                        <div class="row">
+
+                            <div class="col-xl-6 col-md-6">
+                                <div class="card user-card-full" style="height: 150px;">
+                                    <div class="row m-l-0 m-r-0">
+                                        <div class="col-sm-4 bg-c-lite-green user-profile" style="height: 150px;">
+                                            <div class="card-block text-center text-white">
+                                                <h6 class="f-w-600">
+                                                    {{$client->client_name}}
+                                                </h6>
+                                                <button class="btn edit_button" style="color: white;">
+                                                    <i class="feather icon-edit m-t-10 f-16"></i>
+                                                </button>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <div class="card-block">
-                                            <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information
-                                            </h6>
-                                            <div class="row">
-                                                <div class="col-sm-4">
-                                                    <p class="m-b-10 f-w-600">Company</p>
-                                                    <h6 class="text-muted f-w-400">
-                                                        {{$client->client_company}}
-                                                    </h6>
-                                                </div>
-                                                <div class="col-sm-4">
-                                                    <p class="m-b-10 f-w-600">Phone</p>
-                                                    <h6 class="text-muted f-w-400">
-                                                        {{$client->client_phone}}
-                                                    </h6>
-                                                </div>
-                                                <div class="col-sm-4">
-                                                    <p class="m-b-10 f-w-600">Address</p>
-                                                    <h6 class="text-muted f-w-400">
-                                                        {{$client->client_address}}
-                                                    </h6>
+                                        <div class="col-sm-8">
+                                            <div class="card-block">
+                                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information
+                                                </h6>
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <p class="m-b-10 f-w-600">Company</p>
+                                                        <h6 class="text-muted f-w-400">
+                                                            {{$client->client_company}}
+                                                        </h6>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <p class="m-b-10 f-w-600">Phone</p>
+                                                        <h6 class="text-muted f-w-400">
+                                                            {{$client->client_phone}}
+                                                        </h6>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <p class="m-b-10 f-w-600">Address</p>
+                                                        <h6 class="text-muted f-w-400">
+                                                            {{$client->client_address}}
+                                                        </h6>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xl-2 col-md-6">
-                            <div class="card bg-c-green update-card" style="height: 150px;">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col">
-                                            <h4 class="text-white">
-                                                ৳{{$sell->total}}
-                                            </h4>
-                                            <h6 class="text-white m-b-0">Total Bill</h6>
+                            <div class="col-xl-2 col-md-6">
+                                <div class="card bg-c-green update-card" style="height: 150px;">
+                                    <div class="card-block">
+                                        <div class="row align-items-end">
+                                            <div class="col">
+                                                <h4 class="text-white">
+                                                    ৳{{$sell->total}}
+                                                </h4>
+                                                <h6 class="text-white m-b-0">Total Bill</h6>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xl-2 col-md-6">
-                            <div class="card bg-c-yellow update-card" style="height: 150px;">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col">
-                                            <h4 class="text-white">
-                                                ৳{{$sell->received}}
-                                            </h4>
-                                            <h6 class="text-white m-b-0">Received Bill</h6>
+                            <div class="col-xl-2 col-md-6">
+                                <div class="card bg-c-yellow update-card" style="height: 150px;">
+                                    <div class="card-block">
+                                        <div class="row align-items-end">
+                                            <div class="col">
+                                                <h4 class="text-white">
+                                                    ৳{{$sell->received}}
+                                                </h4>
+                                                <h6 class="text-white m-b-0">Received Bill</h6>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xl-2 col-md-6">
-                            <div class="card bg-c-pink update-card" style="height: 150px;">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col">
-                                            <h4 class="text-white">
-                                                ৳{{$sell->total - $sell->received}}
-                                            </h4>
-                                            <h6 class="text-white m-b-0">Due Bill</h6>
+                            <div class="col-xl-2 col-md-6">
+                                <div class="card bg-c-pink update-card" style="height: 150px;">
+                                    <div class="card-block">
+                                        <div class="row align-items-end">
+                                            <div class="col">
+                                                <h4 class="text-white">
+                                                    ৳{{$sell->total - $sell->received}}
+                                                </h4>
+                                                <h6 class="text-white m-b-0">Due Bill</h6>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
+
+
                         </div>
 
 
-
-                    </div>
-
-
-                    <div class="card">
-                        <div class="card-header">
+                        <div class="card">
+                            <div class="card-header">
 
 
-                            <button type="button" class="btn btn-success waves-effect" data-toggle="modal" data-target="#add_modal">Add</button>
-                            <br><br>
+                                <button type="button" class="btn btn-success waves-effect" data-toggle="modal" data-target="#add_modal">Add</button>
+                                <br><br>
 
-                            <!-- @if (Session::has('message'))
+                                <!-- @if (Session::has('message'))
                             <div class="alert {{Session::get('alert-type')}} icons-alert">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <i class="icofont icofont-close-line-circled"></i>
@@ -139,62 +143,69 @@
                             @endif -->
 
 
-                        </div>
+                            </div>
 
 
-                        <div class="card-block">
-                            <div class="dt-responsive table-responsive">
+                            <div class="card-block">
+                                <div class="dt-responsive table-responsive">
 
-                                <table id="datatable" class="table table-striped table-bordered nowrap">
-                                    <thead>
-                                        <tr>
-                                            <th>Date</th>
-                                            <th>Product</th>
-                                            <th>Unit Price</th>
-                                            <th>Total Unit</th>
-                                            <th>Total</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                                    <table id="datatable" class="table table-striped table-bordered nowrap">
+                                        <thead>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Product</th>
+                                                <th>Unit Price</th>
+                                                <th>Total Unit</th>
+                                                <th>Total</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-                                        @foreach ($selldetails as $selldetail)
-                                        <tr>
-                                            <td>
-                                                {{ $selldetail->date }}
-                                            </td>
-                                            <td>
-                                                {{ $selldetail->product_name }}
-                                            </td>
-                                            <td>
-                                                {{ $selldetail->unit_price }}
-                                            </td>
-                                            <td>
-                                                {{ $selldetail->total_unit }}
-                                            </td>
-                                            <td>
-                                                {{ $selldetail->total_unit*$selldetail->unit_price }}
-                                            </td>
-                                            <td>
-                                                <button class="btn btn-primary waves-effect edit_button_details" data-id="{{$selldetail->id}}">Edit</button>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Date</th>
-                                            <th>Product</th>
-                                            <th>Unit Price</th>
-                                            <th>Total Unit</th>
-                                            <th>Total</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </tfoot>
-                                </table>
+                                            @foreach ($selldetails as $selldetail)
+                                            <tr>
+                                                <td>
+                                                    {{ $selldetail->date }}
+                                                </td>
+                                                <td>
+                                                    {{ $selldetail->product_name }}
+                                                </td>
+                                                <td>
+                                                    {{ $selldetail->unit_price }}
+                                                </td>
+                                                <td>
+                                                    {{ $selldetail->total_unit }}
+                                                </td>
+                                                <td>
+                                                    {{ $selldetail->total_unit*$selldetail->unit_price }}
+                                                </td>
+                                                <td>
+                                                    <button class="btn btn-primary waves-effect edit_button_details" data-id="{{$selldetail->id}}">Edit</button>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Product</th>
+                                                <th>Unit Price</th>
+                                                <th>Total Unit</th>
+                                                <th>Total</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
 
+                                </div>
                             </div>
                         </div>
+
+                        <center>
+                            <input type="button" onclick="printDiv('printableArea')" class="btn btn-info" value="Print Invoice" />
+                        </center>
+
+
                     </div>
 
 
@@ -424,6 +435,19 @@
         "info": true,
         "searching": true
     });
+</script>
+
+<script>
+    function printDiv(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        var originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
+    }
 </script>
 
 @endsection
